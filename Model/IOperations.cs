300879ee3f5c44c860a80231655b1dc55c17fa7f
Samuel Rivera <108555378+samuelerm08@users.noreply.github.com>
2022-11-15 14:50:53 -0300

    Add files via upload

---
+++ Model/IOperations.cs
@@ -0,0 +1,15 @@
+﻿namespace CRUDCatalogoWeb.Model
+{
+    public interface IOperations
+    {
+        public List<Articulo> VerTodos();
+        
+        public Articulo VerUno(int id);
+        
+        public void Crear(Articulo a);
+        
+        public void Editar(Articulo a);
+        
+        public void Eliminar(int id);                
+    }
+}
